1d2443eb40230d1eb6035ba96a952145f20876d5
joseRivaldo14 <116230002+joseRivaldo14@users.noreply.github.com>
2022-11-22 23:19:42 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+#include "chico.h"
+#include "grande.h"
+#include <iostream>
+using namespace std;
+
+void menu() {
+    cout << "Dots and Boxes" << endl;
+    cout << "--------------" << endl;
+    cout << "1.Tablero 6x6" << endl;
+    cout << "2.Tablero 10x10" << endl;
+    cout << "0.Salir" << endl;
+    cout << "Seleccione una opcion: ";
+    int opcion;
+    cin >> opcion;
+    cout << endl;
+    while (opcion < 0 || opcion > 2) {
+        cout << "Ingrese una opcion valida: ";
+        cin >> opcion;
+    }
+    if (opcion == 1) {
+        chico tablero1;
+        tablero1.generate_tablero();
+    }
+    if (opcion == 2) {
+        grande tablero2;
+        tablero2.generate_tablero();
+    }
+}
+int main() { menu();
+}
+
